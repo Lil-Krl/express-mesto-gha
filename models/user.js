406e08e92d0a65bd3d37e09d1fde2fd7a0bb38c4
Lil-Krl <Lil-Krl@yandex.ru>
2023-07-27 01:25:40 +0300

    fix userSchema

--- models/user.js
+++ models/user.js
@@ -6,21 +6,19 @@ const AuthErr = require('../errors/AuthErr');
 const userSchema = new mongoose.Schema({
   name: {
     type: String,
-    required: true,
     minlength: 2,
     maxlength: 30,
     default: 'Жак-Ив Кусто',
   },
   about: {
     type: String,
-    required: true,
     minlength: 2,
     maxlength: 30,
     default: 'Исследователь',
   },
   avatar: {
     type: String,
-    required: true,
+    default: 'https://pictures.s3.yandex.net/resources/jacques-cousteau_1604399756.png',
     validate: {
       validator: (url) => validator.isURL(url),
       message: 'Некорректный адрес URL',
@@ -52,7 +50,7 @@ userSchema.statics.findUserByCredentials = function (email, password) {
       return bcrypt.compare(password, user.password)
         .then((matched) => {
           if (!matched) {
-            return Promise.reject(new AuthErr('Неправильная почта или пароль'));
+            return Promise.reject(new AuthErr('Неправильные почта или пароль'));
           }
           return user;
         });
