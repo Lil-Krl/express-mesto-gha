5f4ed726fe33267006641c6f1522f22c3a6ff8c6
Lil-Krl <Lil-Krl@yandex.ru>
2023-07-27 02:48:55 +0300

    another fix

--- routes/users.js
+++ routes/users.js
@@ -15,8 +15,8 @@ const {
 } = require('../middlewares/validation');
 
 userRoutes.get('/', getUsers);
-userRoutes.get('/:userId', validationUserId, getUserById);
 userRoutes.get('/me', getCurrentUser);
+userRoutes.get('/:userId', validationUserId, getUserById);
 userRoutes.patch('/me', validationUpdateUser, updateUser);
 userRoutes.patch('/me/avatar', validationUpdateAvatar, updateAvatar);
 
